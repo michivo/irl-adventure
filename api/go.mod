2da96b40321c9b5e3e8fec68107142b9ec500ab4
Michael Faschinger <mfaschinger@protossecurity.com>
2026-08-31 15:16:00 +0200

    initial commit/scaffolding

---
+++ api/go.mod
@@ -0,0 +1,3 @@
+module github.com/michivo/irl-adventure/api
+
+go 1.23
---
+++ apps/player/src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main>
+    <h1>IRL Adventure</h1>
+    <p>Your next real-world adventure starts here.</p>
+  </main>
+</template>
