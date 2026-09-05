4ff629994565188e42de8d3ffd9cc4dd94d0d9e9
Michael Faschinger <mfaschinger@protossecurity.com>
2026-09-05 14:18:48 +0200

    switch to nuxtui

--- apps/player/src/App.vue
+++ apps/player/src/App.vue
@@ -2,5 +2,7 @@
 </script>
 
 <template>
-  <RouterView />
+  <UApp>
+    <RouterView />
+  </UApp>
 </template>
--- apps/player/src/views/HomeView.vue
+++ apps/player/src/views/HomeView.vue
@@ -9,6 +9,7 @@ const games = useCollection(gamesCollection);
   <main>
     <h1>IRL Adventure</h1>
     <p>Your next real-world adventure starts here. Games:</p>
+    <UAlert title="Ready to play?" />
     {{ games.length ?? 0 }} game(s) available.
     <ul>
       <li v-for="game in games" :key="game.id">{{ game.name }} - {{ game.id }}</li>
