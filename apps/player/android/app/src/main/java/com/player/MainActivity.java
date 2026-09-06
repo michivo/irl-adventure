6078fc95422b3b6ca91f456418d95b7fd5acb736
Michael Faschinger <mfaschinger@protossecurity.com>
2026-09-06 13:47:49 +0200

    Add support for native android app

---
+++ apps/player/android/app/src/main/java/com/player/MainActivity.java
@@ -0,0 +1,5 @@
+package com.player;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ apps/player/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.player',
+  appName: 'player',
+  webDir: 'dist'
+};
+
+export default config;
